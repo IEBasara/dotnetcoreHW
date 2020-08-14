496f42e0ec7755c99add938b7534e798ed8abeee
IEBasara <iebasara@iebasara.net>
2020-08-14 22:20:18 +0800

    First commit

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using hw.Models;
+using SQLitePCL;
+
+namespace hw.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+
+        public DepartmentsController(ContosouniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartment()
+        {
+            return await _context.Department.ToListAsync();
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return department;
+        }
+
+        // PUT: api/Departments/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PutDepartment(int id, Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                // Call the stored procedure
+                await _context.Database.ExecuteSqlRawAsync("dbo.Department_Update @p0, @p1, @p2, @p3, @p4, @p5", 
+                    new object[] { department.DepartmentId, department.Name, department.Budget, department.StartDate, department.InstructorId, department.RowVersion });
+
+                //await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Departments
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Department>> PostDepartment(Department department)
+        {
+            // Call the stored procedure
+            await _context.Database.ExecuteSqlRawAsync("dbo.Department_Insert @p0, @p1, @p2, @p3", new object[] { department.Name, department.Budget, department.StartDate, department.InstructorId });
+
+            //_context.Department.Add(department);
+            //await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Department>> DeleteDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            
+            // Call the stored procedure
+            await _context.Database.ExecuteSqlRawAsync("dbo.Department_Delete @p0, @p1", new object[] { department.DepartmentId, department.RowVersion });
+            //_context.Department.Remove(department);
+            //await _context.SaveChangesAsync();
+
+            return department;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<VwDepartmentCourseCount>>> GetDepartmentCourseCount()
+        {
+            var datalist = await _context.VwDepartmentCourseCount.FromSqlRaw("SELECT DepartmentId, Name, CourseCount FROM dbo.vwDepartmentCourseCount").ToListAsync();
+
+            if (datalist == null)
+            {
+                return NotFound();
+            }
+
+            return datalist;
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return _context.Department.Any(e => e.DepartmentId == id);
+        }
+    }
+}
---
+++ Models/ContosouniversityContext.partial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw.Models
+{
+    public partial class ContosouniversityContext
+    {
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+           CancellationToken cancellationToken = default(CancellationToken)
+        )
+        {
+            OnBeforeSaving();
+            return (await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken));
+        }
+
+        private void OnBeforeSaving()
+        {
+            var entries = ChangeTracker.Entries();
+            var utcNow = DateTime.UtcNow;
+            dynamic entity = null;
+            foreach (var entry in entries)
+            {
+                entity = entry.Entity;
+                if (entity is Course || // If these class is inherited from the same interface, then this will be simpler.
+                    entity is Department ||
+                    entity is Person)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                        case EntityState.Added:
+                            entity.DateModified = utcNow;
+                            break;
+                        case EntityState.Deleted:
+                            entry.State = EntityState.Modified;
+                            entity.IsDeleted = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
